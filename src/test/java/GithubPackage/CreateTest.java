39bc116f28f3742939c0b71f710892754905f31d
Asus <Asus@LAPTOP-5CVCD5UV>
2022-09-27 15:55:47 +0530

    updated new test

--- src/test/java/GithubPackage/CreateTest.java
+++ src/test/java/GithubPackage/CreateTest.java
@@ -4,8 +4,11 @@ import org.testng.annotations.Test;
 
 public class CreateTest {
 	@Test
-	public void Create() 
+	public void updated() 
 	{
       System.out.println("Created");
+      System.out.println("modified");
+      System.out.println("inserted");
+
 }
 }
