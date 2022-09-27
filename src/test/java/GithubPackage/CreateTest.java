64018219dcc7975ebee172e3a7782130dc2d8ba3
Asus <Asus@LAPTOP-5CVCD5UV>
2022-09-27 16:02:50 +0530

    updated to push to master

--- src/test/java/GithubPackage/CreateTest.java
+++ src/test/java/GithubPackage/CreateTest.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 public class CreateTest {
 	@Test
-	public void updated() 
+	public void updateddd() 
 	{
       System.out.println("Created");
       System.out.println("modified");
